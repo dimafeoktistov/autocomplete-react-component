ae0b10dece9e5dc70fe36fc46369084127e2fefe
Dima Feoktistov <feoktistovdmitriy@gmail.com>
2018-11-05 17:04:18 +0700

    Feat: Added basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Autocomplete from "./Autocomplete/Autocomplete";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Autocomplete
+            suggestions={[
+              "Корова",
+              "Собака",
+              "Буйвол",
+              "Ежик",
+              "Воробей",
+              "Крокодил",
+              "Рептилия",
+              "Акула",
+              "Хвост",
+              "Папоротник"
+            ]}
+          />
         </header>
       </div>
     );
